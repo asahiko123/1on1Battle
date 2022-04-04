636fbcad5a42f8cba686f1f13bd4f8683f2e560d
asahiko123 <66983728+asahiko123@users.noreply.github.com>
2022-04-04 13:03:02 +0900

    topPage作成

---
+++ resources/views/top.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.layout')
+
+@section('content')
+<div class="loginPage">
+    <div class="container">
+        <div class="loginPage_contents">
+            <h1 class="h3 loginPage_contents_title">1ON1 Battle</h1>
+            <div class="btn loginPage_contents_btn"><a href="{{route('login')}}" class="text-white">メールアドレスでログインする</a></div>
+        </div>
+    </div>
+</div>
