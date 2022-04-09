fedf02279d8f5eb2357e12baacf5bd6228fe9a44
asahiko123 <66983728+asahiko123@users.noreply.github.com>
2022-04-09 16:20:20 +0900

    top画面変更,web.php変更

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -4,8 +4,9 @@
 <div class="loginPage">
     <div class="container">
         <div class="loginPage_contents">
-            <h1 class="h3 loginPage_contents_title">1ON1 Battle</h1>
-            <div class="btn loginPage_contents_btn"><a href="{{route('login')}}" class="text-white">メールアドレスでログインする</a></div>
+            <h1 class="h3 loginPage_contents_title">飴ちゃんセレクター</h1>
+            <p>いま、あなたに必要な飴ちゃんは何かね？</p>
+            <div class="btn loginPage_contents_btn"><a href="{{route('login')}}" class="text-white">はじめる</a></div>
         </div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,10 @@ Route::get('/', function () {
     return view('top');
 });
 
+Route::get('show/{id}','UserController@show')->name('users.show');
+Route::get('edit/{id}', 'UserController@edit')->name('users.edit');
+Route::post('update/{id}', 'UserController@update')->name('users.update');
 
-Route::group(['prefix' => 'users','middleware' => 'auth'],function(){
-    Route::get('show/{id}','UserController@show')->name('users.show');
-    Route::get('edit/{id}', 'UserController@edit')->name('users.edit');
-    Route::post('update/{id}', 'UserController@update')->name('users.update');
-});
 
 Auth::routes();
 
