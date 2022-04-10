238af1fafc3460daa0d542fcc412a6acfdc193eb
asahiko123 <66983728+asahiko123@users.noreply.github.com>
2022-04-10 12:00:50 +0900

    Jtinder導入

--- database/seeds/QuestionTableSeeder.php
+++ database/seeds/QuestionTableSeeder.php
@@ -12,14 +12,38 @@ class QuestionTableSeeder extends Seeder
     public function run()
     {
         DB::table('question')->insert([
-            ['statement' => '最近癒しが足りないと感じるかい？'],
-            ['statement' => 'スースーする飴ちゃんは苦手かい？'],
-            ['statement' => '濃い～味で甘いやつ欲しいかい？'],
-            ['statement' => '高級感のある飴ちゃんはどうだい？'],
-            ['statement' => '酸っぱいのが好きかい？'],
-            ['statement' => 'もしかして風邪ぎみかい？'],
-            ['statement' => 'ゲームしながら食べるのかい？'],
-            ['statement' => '大玉の飴ちゃん欲しくないかい？']
+            [
+                'statement' => '最近癒しが足りないと感じるかい？',
+                'card_image'=> '150x150.png'
+            ],
+            [
+                'statement' => 'スースーする飴ちゃんは苦手かい？',
+                'card_image'=> '150x150.png'
+            ],
+            [
+                'statement' => '濃い～味で甘いやつ欲しいかい？',
+                'card_image'=> '150x150.png'
+            ],
+            [
+                'statement' => '高級感のある飴ちゃんはどうだい？',
+                'card_image'=> '150x150.png'
+            ],
+            [
+                'statement' => '酸っぱいのが好きかい？',
+                'card_image'=> '150x150.png'
+            ],
+            [
+                'statement' => 'もしかして風邪ぎみかい？',
+                'card_image'=> '150x150.png'
+            ],
+            [
+                'statement' => 'ゲームしながら食べるのかい？',
+                'card_image'=> '150x150.png'
+            ],
+            [
+                'statement' => '大玉の飴ちゃん欲しくないかい？',
+                'card_image'=> '150x150.png'
+                ]
         ]);
     }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -3,11 +3,19 @@
 @section('content')
 <div class="loginPage">
     <div class="container">
-        <div class="loginPage_contents">
+        <div id="tinderslide">
             <h1 class="h3 loginPage_contents_title">飴ちゃんセレクター</h1>
-            @foreach($questions as $question)
-                {{$question->statement}}
-            @endforeach
+            <ul>
+                @foreach($questions as $question)
+                <li data-user_id="{{ $question->id }}">
+                  <div class="userName">{{ $question->statement }}</div>
+                  <img src="/storage/images/{{ $question->card_image}}">
+                  <div class="like"></div>
+                  <div class="dislike"></div>
+                </li>
+                @endforeach
+            </ul>
+            <div class="noUser">近くにお相手がいません。</div>
         </div>
     </div>
 </div>
