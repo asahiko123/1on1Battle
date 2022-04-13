dbf49a9680c5ed32e54d4dc06e13fcdac370f504
asahiko123 <66983728+asahiko123@users.noreply.github.com>
2022-04-13 12:02:23 +0900

    QuestionControllerリファクタリング

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,9 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Question;
-use App\Services\FileUploadServices;
-use App\Services\CheckExtensionServices;
-use Intervention\Image\Facades\Image;
+use App\Services\CardImagesUploadServices;
 
 
 class QuestionController extends Controller
@@ -56,20 +54,7 @@ class QuestionController extends Controller
         
         $question = new Question;
 
-        if(!is_null($request['card_image'])){
-            $imageFile = $request['card_image'];
-
-            $list = FileUploadServices::fileUpload($imageFile);
-            list($extension,$fileNameToStore,$fileData) = $list;
-
-            $data_url = CheckExtensionServices::checkExtension($fileData,$extension);
-
-            $image = Image::make($data_url);
-
-            $image->resize(400,400)->save(storage_path(). '/app/public/images/' . $fileNameToStore);
-
-            $question->card_image = $fileNameToStore;
-        }
+        CardImagesUploadServices::cardImagesUpload($request);
 
         $question->statement = $request->statement;
         
@@ -114,20 +99,7 @@ class QuestionController extends Controller
     {
         $question = Question::find($id);
 
-        if(!is_null($request['card_image'])){
-            $imageFile = $request['card_image'];
-
-            $list = FileUploadServices::fileUpload($imageFile);
-            list($extension,$fileNameToStore,$fileData) = $list;
-
-            $data_url = CheckExtensionServices::checkExtension($fileData,$extension);
-
-            $image = Image::make($data_url);
-
-            $image->resize(400,400)->save(storage_path(). '/app/public/images/' . $fileNameToStore);
-
-            $question->card_image = $fileNameToStore;
-        }
+        CardImagesUploadServices::cardImagesUpload($request);
 
         $question->statement = $request->statement;
         
---
+++ app/Services/CardImagesUploadServices.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services;
+
+use App\Services\FileUploadServices;
+use App\Services\CheckExtensionServices;
+use Intervention\Image\Facades\Image;
+use Illuminate\Http\Request;
+
+
+class CardImagesUploadServices{
+
+    public static function cardImagesUpload(Request $request){
+
+        if(!is_null($request['card_image'])){
+            $imageFile = $request['card_image'];
+
+            $list = FileUploadServices::fileUpload($imageFile);
+            list($extension,$fileNameToStore,$fileData) = $list;
+
+            $data_url = CheckExtensionServices::checkExtension($fileData,$extension);
+
+            $image = Image::make($data_url);
+
+            $image->resize(400,400)->save(storage_path(). '/app/public/images/' . $fileNameToStore);
+
+            $question->card_image = $fileNameToStore;
+        }
+    }
+}
