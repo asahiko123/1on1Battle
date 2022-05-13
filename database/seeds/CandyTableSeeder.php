616cc296c61ed18bb942e084bd74c03cc755f73c
asahiko123 <66983728+asahiko123@users.noreply.github.com>
2022-05-13 11:34:36 +0900

    結果表示画面:HTML構造の修正

--- database/seeds/CandyTableSeeder.php
+++ database/seeds/CandyTableSeeder.php
@@ -30,7 +30,7 @@ class CandyTableSeeder extends Seeder
 
                 'name'  => 'パインアメ',
                 'maker' => 'パイン',
-                'style' => 'さっぱり',
+                'style' => 'フルーティー',
                 'taste' => '果物',
                 'url'   => 'https://www.amazon.co.jp/UHA%E5%91%B3%E8%A6%9A%E7%B3%96-%E7%89%B9%E6%BF%83%E3%83%9F%E3%83%AB%E3%82%AF8-2-%E6%BF%83%E9%A6%99%E3%81%84%E3%81%A1%E3%81%94-75g%C3%974%E8%A2%8B/dp/B08KXMLHM2/ref=sr_1_110?keywords=%E9%A3%B4&qid=1649769121&sr=8-110'
 
@@ -41,14 +41,14 @@ class CandyTableSeeder extends Seeder
                 'style' => 'さっぱり',
                 'taste' => '果物',
                 'url'   => 'https://www.amazon.co.jp/%EF%BC%B5%EF%BC%A8%EF%BC%A1%E5%91%B3%E8%A6%9A%E7%B3%96-%E9%82%AA%E6%89%95%E3%81%AE%E3%81%A9%E9%A3%B4-%E6%9F%91%E6%A9%98%E3%83%9F%E3%83%83%E3%82%AF%E3%82%B9-72g-%C3%976%E8%A2%8B/dp/B07YSNBLMF/ref=sr_1_84?keywords=%E9%A3%B4&qid=1649770681&sr=8-84'
-                
+
             ],[
 
                 'name'  => 'レモンスカッシュ',
                 'maker' => '不二家',
-                'style' => 'さっぱり',
+                'style' => '酸っぱい',
                 'taste' => '果物',
-                'url'   => 'https://www.amazon.co.jp/UHA%E5%91%B3%E8%A6%9A%E7%B3%96-%E7%89%B9%E6%BF%83%E3%83%9F%E3%83%AB%E3%82%AF8-2-%E6%BF%83%E9%A6%99%E3%81%84%E3%81%A1%E3%81%94-75g%C3%974%E8%A2%8B/dp/B08KXMLHM2/ref=sr_1_110?keywords=%E9%A3%B4&qid=1649769121&sr=8-110'
+                'url'   => 'https://www.amazon.co.jp/%E4%B8%8D%E4%BA%8C%E5%AE%B6-%E3%83%AC%E3%83%A2%E3%83%B3%E3%82%B9%E3%82%AB%E3%83%83%E3%82%B7%E3%83%A5%E3%82%AD%E3%83%A3%E3%83%B3%E3%83%87%E3%82%A3%E8%A2%8B-80g%C3%976%E8%A2%8B/dp/B007EKQO2C/ref=sr_1_7?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=20PQM1H87I2KO&keywords=%E3%83%AC%E3%83%A2%E3%83%B3%E3%82%B9%E3%82%AB%E3%83%83%E3%82%B7%E3%83%A5&qid=1652408153&sprefix=%E3%83%AC%E3%83%A2%E3%83%B3%E3%82%B9%E3%82%AB%E3%83%83%E3%82%B7%E3%83%A5%2Caps%2C178&sr=8-7'
             ],[
                 'name'  => '北見ハッカ飴',
                 'maker' => '北見ハッカ通商',
--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -8,28 +8,28 @@
   <div class='container'>
 
     <form class="form mt-5" method="POST" action="{{ route('admin.register') }}" enctype="multipart/form-data">
-    @csrf
+        @csrf
 
-    <div class="form-group @error('name')has-error @enderror">
-        <label>名前</label>
-        <input type="name" name="name" class="form-control" placeholder="名前を入力してください">
-        @error('name')
-            <span class="errorMessage">
-            {{ $message }}
-            </span>
-        @enderror
-    </div>
+        <div class="form-group @error('name')has-error @enderror">
+            <label>名前</label>
+            <input type="name" name="name" class="form-control" placeholder="名前を入力してください">
+            @error('name')
+                <span class="errorMessage">
+                {{ $message }}
+                </span>
+            @enderror
+        </div>
 
 
-    <div class="form-group @error('email')has-error @enderror">
-    <label>メールアドレス</label>
-    <input type="email" name="email" class="form-control" placeholder="メールアドレスを入力してください">
-    @error('email')
-        <span class="errorMessage">
-            {{ $message }}
-        </span>
-    @enderror
-    </div>
+        <div class="form-group @error('email')has-error @enderror">
+            <label>メールアドレス</label>
+            <input type="email" name="email" class="form-control" placeholder="メールアドレスを入力してください">
+            @error('email')
+                <span class="errorMessage">
+                    {{ $message }}
+                </span>
+            @enderror
+        </div>
 
         <div class="form-group @error('password')has-error @enderror">
             <label>パスワード</label>
@@ -47,14 +47,12 @@
             <input type="password" name="password_confirmation" class="form-control" placeholder="パスワードを再度入力してください">
         </div>
 
+        <div class="text-center">
+            <button type="submit" class="btn submitBtn">はじめる</button>
+            <div class="linkToLogin">
+                <a href="{{ route('admin.login.index') }}">ログインはこちら</a>
+            </div>
         </div>
-
-      <div class="text-center">
-      <button type="submit" class="btn submitBtn">はじめる</button>
-      <div class="linkToLogin">
-        <a href="{{ route('admin.login.index') }}">ログインはこちら</a>
-      </div>
-      </div>
     </form>
   </div>
 </div>
